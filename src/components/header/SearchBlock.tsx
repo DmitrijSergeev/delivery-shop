8c6cb7fd24a03edf31711de90b9cdcb6699288f0
Dmitry Sergeev <dmitrijsergeev@gmail.com>
2025-06-27 20:28:07 +0400

    refactoring button-header

--- src/components/header/SearchBlock.tsx
+++ src/components/header/SearchBlock.tsx
@@ -3,11 +3,13 @@ import Image from "next/image";
 
 export const SearchBlock = () => {
     return (
-        <div className={'flex flex-row'}>
-            <button className={'bg-(--color-primary) ' +
-                'hover:shadow-(--shadow-button-default) active:shadow-(--shadow-button-active)'}
+        <div className={'flex flex-row gap-4 flex-grow'}>
+            <button className={'bg-(--color-primary) hover:shadow-(--shadow-button-default) active:shadow-(--shadow-button-active) hidden md:flex w-10 p-2 gap-4 lg:w-35 cursor-pointer duration-300 rounded'}
             >
-                <Image src={'/icon/icon-menu.svg'} alt={'icon'}/>
+                <Image src={'/icons-header/icon-menu.svg'} alt={'icon-menu'}
+                       width={24} height={24}
+                       className={'hidden md:block'}
+                />
             </button>
             <div></div>
         </div>
