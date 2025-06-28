39ceeef5cbeb9791e763783d5e8e5768f70071a4
Dmitry Sergeev <dmitrijsergeev@gmail.com>
2025-06-28 16:28:51 +0400

    header ending

--- src/components/header/LogoBlock.tsx
+++ src/components/header/LogoBlock.tsx
@@ -1,20 +1,27 @@
-import React from 'react';
 import Link from "next/link";
 import Image from "next/image";
 
-export const LogoBlock = () => {
+const LogoBlock = () => {
     return (
-        <Link href="/" className={'flex flex-row gap-3 items-center cursor-pointer'}>
-            <div className={'relative w-10 h-8 md:w-12 md:h-10 xl:w-10 xl:h-8'}>
-                <Image src={'/icons-header/icon-logo.svg'} alt={'Logo'}
-                       fill sizes={'(max-width: 768px) 100vw, max-width: 1200px) 50vw, 33vw'}
+        <Link href="/" className="flex flex-row gap-3 items-center cursor-pointer">
+            <div className="relative w-10 h-8 md:w-12 md:h-10 xl:w-10 xl:h-8">
+                <Image
+                    src="/icons-header/icon-logo.svg"
+                    alt="Логотип"
+                    fill
+
                 />
             </div>
-            <div className={'relative hidden md:block w-25 h-3'}>
-                <Image src={'/icons-header/icon-text.png'} alt={'text'}
-                       fill sizes={'100px'}
+            <div className="relative hidden lg:block w-25 h-3">
+                <Image
+                    src="/icons-header/icon-text.png"
+                    alt="Северяночка"
+                    fill
+                    sizes="100px"
                 />
             </div>
         </Link>
     );
 };
+
+export default LogoBlock;
