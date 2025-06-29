309c5ad707a1fd807a60fbad9b2b9ca9e9b5b737
Dmitry Sergeev <dmitrijsergeev@gmail.com>
2025-06-29 21:20:13 +0400

    calculate final price

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,14 @@
 import Slider from "@/components/slider/Slider";
+import Actions from "@/components/Actions";
 
 export default function Home() {
     return (
         <main className={'w-full mx-auto mb-20'}>
             <Slider/>
+            <div className={'px-[max(12px, calc((100% -1208px)/2))] ' +
+                'flex flex-col gap-y-20 md:mb-2.5 xl:mb-30'}>
+                <Actions/>
+            </div>
         </main>
     );
 }
